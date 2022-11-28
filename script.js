71d83399d5d1680de20eb7a4ab3c04b66f29bd43
Exalted97 <119164142+Exalted97@users.noreply.github.com>
2022-11-29 00:59:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const el = document.getElementById('el');
-el.scrollIntoView();
+const smoothLinks = document.querySelectorAll('a[href^="#scroll"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
